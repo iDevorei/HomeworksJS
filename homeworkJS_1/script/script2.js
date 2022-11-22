f3c5ed77b770c20a1891415e7f8abe6bb120494f
iDevorei <109354693+iDevorei@users.noreply.github.com>
2022-11-22 20:14:38 +0300

    Delete homeworkJS_1/script directory

--- homeworkJS_1/script/script2.js
+++
@@ -1,7 +0,0 @@
-//Задание 3
-
-let width = 10,
-    height = 23;
-
-let SPryam = width * height;
-console.log (SPryam, typeof SPryam);
--- homeworkJS_1/script/script6.js
+++
@@ -1,9 +0,0 @@
-// задание 7
-
-let S = 2000000,
-    p = 0.10,
-    years = 5,
-    month = years *12;
-
-let  Pereplata = S * p / (1 - (1 / (1 + p)) * month);
-console.log (Pereplata, typeof Pereplata);
